740926a10fee86a67907d6495ad5ba5e29e98128
Zerixxx8995 <manavamin99zx@gmail.com>
2026-08-05 22:04:15 +0530

    fix: import path in metro.config.js

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -1,5 +1,6 @@
 // Learn more https://docs.expo.dev/guides/customizing-metro
 const { getDefaultConfig } = require('expo/metro-config');
+const path = require('path');
 
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
