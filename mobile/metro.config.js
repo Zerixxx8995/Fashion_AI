0dd7eaddc5799e11a4e3aae0e1cb14dff84f672e
Zerixxx8995 <manavamin99zx@gmail.com>
2026-08-05 22:01:47 +0530

    fix: inject polyfills.js statically before InitializeCore in Metro serializer

--- mobile/metro.config.js
+++ mobile/metro.config.js
@@ -4,4 +4,14 @@ const { getDefaultConfig } = require('expo/metro-config');
 /** @type {import('expo/metro-config').MetroConfig} */
 const config = getDefaultConfig(__dirname);
 
+config.serializer = {
+  ...config.serializer,
+  getModulesRunBeforeMainModule: (entryPoint) => [
+    path.join(__dirname, 'polyfills.js'),
+    require.resolve('react-native/Libraries/Core/InitializeCore.js'),
+    require.resolve('expo/src/winter/index.ts'),
+    require.resolve('@expo/metro-runtime/src/index.ts'),
+  ],
+};
+
 module.exports = config;
